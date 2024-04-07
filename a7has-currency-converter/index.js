adff6ce202591903c532efd42f6451204aabad70
HasnainAbass <hasnainabass243@gmail.com>
2024-04-07 15:58:57 +0500

    initial files!

---
+++ a7has-currency-converter/index.js
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const currency = {
+    USD: 1, // Base currency !
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280
+};
+let userAnswer = await inquirer.prompt([
+    {
+        message: "Enter From Currency",
+        name: "from",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    {
+        message: "Enter To Currency",
+        name: "to",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    {
+        message: "Enter your Amount :",
+        name: "amount",
+        type: "number"
+    }
+]);
+// logical statments!
+let fromAmount = currency[userAnswer.from]; // Exchange Rate !
+let toAmount = currency[userAnswer.to]; //  Exchange Rate !
+let amount = userAnswer.amount;
+let baseAmount = amount / fromAmount; // US Base Currency // line no 6 !
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
